6ea72b6ffd74b08320ba476615ac186c2f246089
Sunny <sunny.ahmed1703101@gmail.com>
2024-12-12 23:32:25 +0600

    Ostad

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1) Write a function named calculateDifference that takes two arguments and returns the difference between the first and second arguments.
+
+
+function calculateDifference(a,b){
+    return a-b;
+}
+
+console.log(calculateDifference(5,3));
+
+
+// 2) Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
+
+const result = (function(x, y) {
+
+    return x + y;
+
+})(5, 10);
+
+console.log(result);
