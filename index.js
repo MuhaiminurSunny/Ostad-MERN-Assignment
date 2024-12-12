32645a58d26db74c5b49f958612fb0665225b2e4
Sunny <sunny.ahmed1703101@gmail.com>
2024-12-12 23:41:03 +0600

    2nd

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ console.log(calculateDifference(5,3));
 
 // 2) Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.
 
-const result = (function(x, y) {
+function isOdd(a){
+    if(a%2===0){
+        console.log("True");
+    }
+    else{
+        console.log("False");
+    }
+}
 
-    return x + y;
-
-})(5, 10);
-
-console.log(result);
+isOdd(4)
